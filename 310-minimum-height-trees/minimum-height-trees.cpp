b13221bda46bc8656833267502fbd6e771f9152f
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-11 22:39:34 +0530

    Time: 23 ms (98.97%) | Memory: 56.1 MB (96.99%) - LeetSync

---
+++ 310-minimum-height-trees/minimum-height-trees.cpp
@@ -0,0 +1,45 @@
+// Minimum Height Trees - optimized (topological leaf removal)
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if (n == 1) return {0};
+
+        vector<vector<int>> adj(n);
+        vector<int> degree(n, 0);
+
+        for (auto &e : edges) {
+            int u = e[0], v = e[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+            degree[u]++; degree[v]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < n; ++i) {
+            if (degree[i] == 1) q.push(i); // leaf
+        }
+
+        int remaining = n;
+        while (remaining > 2) {
+            int leafCount = q.size();
+            remaining -= leafCount;
+            for (int i = 0; i < leafCount; ++i) {
+                int leaf = q.front(); q.pop();
+                for (int nei : adj[leaf]) {
+                    degree[nei]--;
+                    if (degree[nei] == 1) q.push(nei);
+                }
+            }
+        }
+
+        vector<int> ans;
+        while (!q.empty()) {
+            ans.push_back(q.front());
+            q.pop();
+        }
+        return ans;
+    }
+};
