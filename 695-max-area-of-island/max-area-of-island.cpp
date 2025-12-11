14c572ba2143f21a506e91f7c0a0e14d4acfeb3e
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-11 11:44:18 +0530

    Time: 3 ms (55.50%) | Memory: 29.1 MB (34.31%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,53 +1,132 @@
-class Solution {
+class DisJointSet
+{
 public:
-    bool isValid(int r, int c, int m, int n)
-    {
-        return (r>=0 && c>=0 && r<m && c<n);
-    }
+    vector<int> parent;
+    vector<int> rank;
+    vector<int> size;
 
-    void dfs(int row, int col, int m, int n,
-    vector<vector<int>> &vis, int &cnt, int dx[], int dy[],
-    vector<vector<int>> &v)
+    DisJointSet(int n)
     {
-        vis[row][col] = 1;
-        cnt++;
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
 
-        for(int i=0; i<4; i++)
+        for (int i = 0; i <= n; i++)
         {
-            int nr = row+dx[i];
-            int nc = col+dy[i];
-
-            if(isValid(nr,nc,m,n))
-            {
-                if(!vis[nr][nc] && v[nr][nc]==1)
-                {
-                    dfs(nr,nc,m,n,vis,cnt,dx,dy,v);
-                }
-            }
+            parent[i] = i;
+            size[i] = 1;
         }
     }
+
+    int FindUpar(int node)
+    {
+        if (node == parent[node])
+        {
+            return node;
+        }
+
+        return parent[node] = FindUpar(parent[node]);
+    }
+
+    void UnionByRank(int u, int v)
+    {
+        int uu = FindUpar(u);
+        int uv = FindUpar(v);
+
+        if (uu == uv)
+        {
+            return;
+        }
+        if (rank[uu] < rank[uv])
+        {
+            parent[uu] = uv;
+        }
+        else if (rank[uu] > rank[uv])
+        {
+            parent[uv] = uu;
+        }
+        else
+        {
+            parent[uv] = uu;
+            rank[uu]++;
+        }
+    }
+
+    void UnionBySize(int u, int v)
+    {
+        int uu = FindUpar(u);
+        int uv = FindUpar(v);
+
+        if (uu == uv)
+        {
+            return;
+        }
+
+        if (size[uu] < size[uv])
+        {
+            parent[uu] = uv;
+            size[uv] += size[uu];
+        }
+        else
+        {
+            parent[uv] = uu;
+            size[uu] += size[uv];
+        }
+    }
+};
+
+class Solution {
+public:
+    bool isValid(int r, int c, int m, int n) {
+        return r >= 0 && c >= 0 && r < m && c < n;
+    }
     int maxAreaOfIsland(vector<vector<int>>& v) {
-        int m =  v.size();
+        int m = v.size();
         int n = v[0].size();
 
-        vector<vector<int>> vis(m, vector<int> (n,0));
-        int maxi = 0;
+        DisJointSet ds(m*n);
         int dx[] = {-1,0,1,0};
         int dy[] = {0,1,0,-1};
+        int ans = 0;
 
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
             {
-                if(v[i][j]==1 && !vis[i][j])
+                int node = i*n + j;
+                if(v[i][j]==1)
                 {
-                    int cnt = 0;
-                    dfs(i,j,m,n,vis,cnt,dx,dy,v);
-                    maxi = max(maxi, cnt);
+                    for(int k=0; k<4; k++)
+                    {
+                        int nr = i+dx[k];
+                        int nc = j+dy[k];
+
+                        int adnode = nr*n + nc;
+
+                        if(isValid(nr,nc,m,n) && v[nr][nc]==1)
+                        {
+                            if(ds.FindUpar(node) != ds.FindUpar(adnode))
+                            {
+                                ds.UnionBySize(node,adnode);
+                            }
+                        }
+                    }
                 }
             }
         }
 
-        return maxi;
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(v[i][j]==1)
+                {
+                    int node = i*n+j;
+                    ans=max(ans,ds.size[node]);
+                }
+            }
+        }
+
+        return ans;
     }
 };
