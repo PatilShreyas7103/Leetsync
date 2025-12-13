148a7eb4eca9658f8cdb9f1cbcee5d923e44917c
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-14 00:28:11 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (50.17%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,27 +1,6 @@
 class Solution {
 public:
-    int solve(int r, int c, vector<vector<int>> &v, vector<vector<int>> &dp)
-    {
-        if(r<0 || c<0 || c>r)
-        {
-            return INT_MAX;
-        }
 
-        if(r==0)
-        {
-            return dp[r][c] = v[0][0];
-        }
-
-        if(dp[r][c]!=-1)
-        {
-            return dp[r][c];
-        }
-
-        int f1 = solve(r-1,c,v,dp);
-        int f2 = solve(r-1,c-1,v,dp);
-
-        return dp[r][c] = v[r][c]+ min(f1,f2);
-    }
     int minimumTotal(vector<vector<int>>& v) {
         int m = v.size();
 
