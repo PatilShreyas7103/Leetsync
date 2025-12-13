dd37161cb3783653c1d80971faec9d09865eb054
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-13 11:25:06 +0530

    Time: 3 ms (62.50%) | Memory: 8.3 MB (11.44%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -6,6 +6,15 @@ public:
             return n;
         }
 
-        return fib(n-2)+fib(n-1);
+        vector<int> dp(n+1,0);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++)
+        {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
     }
 };
