73ba97f31ffd246ed9842e1edd07fb981267add4
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-13 11:21:27 +0530

    Time: 8 ms (47.29%) | Memory: 7.8 MB (37.15%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int solve(int n)
+    {
+        if(n==0 || n==1)
+        {
+            return n;
+        }
+
+        return solve(n-2)+solve(n-1);
+    }
+    int fib(int n) {
+        return solve(n);
+    }
+};
