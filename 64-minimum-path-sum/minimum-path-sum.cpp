cdc1e0214f879d67b12eaa0af02e0a019babd508
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-13 21:44:55 +0530

    Time: 2 ms (34.49%) | Memory: 16.2 MB (67.11%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>> &v, vector<vector<int>> &dp)
-    {
-        if(i<0 || j<0)
-        {
-            return INT_MAX;
-        }
 
-        if(i==0 && j==0)
-        {
-            return dp[i][j] = v[0][0];
-        }
-
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-
-        int up = solve(i-1,j,v,dp);
-        int left = solve(i,j-1,v,dp);
-
-        return dp[i][j] =  v[i][j]+ min(up, left);
-    }
     int minPathSum(vector<vector<int>>& v) {
         int m = v.size();
         int n = v[0].size();
 
-        vector<vector<int>> dp(m, vector<int> (n,-1));
+        vector<vector<int>> dp(m, vector<int> (n,0));
 
-        return solve(m-1,n-1,v,dp);
+        dp[0][0] = v[0][0];
+        for(int i=1; i<m; i++)
+        {
+            dp[i][0] = dp[i-1][0]+v[i][0];
+        }
+
+        for(int i=1; i<n; i++)
+        {
+            dp[0][i] = dp[0][i-1]+v[0][i];
+        }
+
+        for(int i=1; i<m; i++)
+        {
+            for(int j=1; j<n; j++)
+            {
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1])+ v[i][j];
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
