c38e0e825b5aca5a5110614b7ac04883adebcae0
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-13 21:10:52 +0530

    Time: 3 ms (10.61%) | Memory: 11.9 MB (26.17%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -38,7 +38,44 @@ public:
             return 0;
         }
 
-        vector<vector<int>> dp(m, vector<int> (n,-1));
-        return solve(m-1,n-1,v,dp);
+        vector<vector<int>> dp(m, vector<int> (n,0));
+        for(int i=0; i<m; i++)
+        {
+            if(v[i][0]==1)
+            {
+                break;
+            }
+            dp[i][0] = 1;
+        }
+        for(int i=0; i<n; i++)
+        {
+            if(v[0][i]==1)
+            {
+                break;
+            }
+            dp[0][i] = 1;
+        }
+
+        for(int i=1; i<m; i++)
+        {
+            for(int j=1; j<n; j++)
+            {
+                int up = dp[i-1][j];
+                int left = dp[i][j-1];
+
+                if(v[i][j]==1)
+                {
+                    dp[i][j]=0;
+                }
+                else
+                {
+                    dp[i][j] = up+left;
+                }
+                
+            }
+        }
+
+
+        return dp[m-1][n-1];
     }
 };
