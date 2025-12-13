ccbac521e62967bf7bd5c1b40a9681550fe0373d
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-14 00:25:35 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (34.36%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -26,10 +26,25 @@ public:
         int m = v.size();
 
         int ans = INT_MAX;
-        vector<vector<int>> dp(m, vector<int> (m, -1));
+        vector<vector<int>> dp(m, vector<int> (m, INT_MAX));
+
+        dp[0][0] = v[0][0];
+        for(int i=1; i<m; i++)
+        {
+            dp[i][0] = dp[i-1][0]+v[i][0];
+        }
+
+        for(int i=1; i<m; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                dp[i][j] = min(dp[i-1][j], dp[i-1][j-1])+v[i][j];
+            }
+        }
+
         for(int i=0; i<m; i++)
         {
-            ans=min(ans, solve(m-1,i,v,dp));
+            ans=min(ans, dp[m-1][i]);
         }
 
         return ans;
