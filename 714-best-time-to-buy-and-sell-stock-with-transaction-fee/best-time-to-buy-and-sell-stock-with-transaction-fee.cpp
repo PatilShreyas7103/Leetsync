318c8d17fd8e3465008623307cf2e11b87793523
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-16 00:43:52 +0530

    Time: 104 ms (34.77%) | Memory: 95.1 MB (41.00%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    int solve(int i, int buy, vector<int> &v, int n,
-    vector<vector<int>> &dp, int f)
-    {
-        if(i==n)
-        {
-            return 0;
-        }
 
-        if(dp[i][buy]!=-1)
-        {
-            return dp[i][buy];
-        }
-
-        if(buy)
-        {
-            return dp[i][buy] = max(solve(i+1,buy,v,n,dp,f), -v[i]+solve(i+1,!buy,v,n,dp,f));
-        }
-        else
-        {
-            return dp[i][buy] = max(solve(i+1,buy,v,n,dp,f), v[i]-f+solve(i+1,!buy,v,n,dp,f));
-        }
-    }
     int maxProfit(vector<int>& v, int f) {
         int n = v.size();
-        vector<vector<int>> dp(n, vector<int> (2,-1));
-        return solve(0,1,v,n,dp,f);
+        vector<vector<int>> dp(n+1, vector<int> (2,0));
+        for(int i=n-1; i>=0; i--)
+        {
+            for(int j=1; j>=0; j--)
+            {
+                if(j==1)
+                {
+                    // buy
+                    dp[i][j] = max(dp[i+1][j], -v[i]+dp[i+1][!j]);
+                }   
+                else
+                {
+                    // sell
+                    dp[i][j] = max(dp[i+1][j], v[i]-f+dp[i+1][!j]);
+                }
+            }
+        }
+
+        return dp[0][1];
     }
 };
