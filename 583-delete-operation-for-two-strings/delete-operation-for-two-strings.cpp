e7b296d62f7b9a1c7f44a7b2acbb81423fef6003
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-15 12:43:46 +0530

    Time: 11 ms (63.92%) | Memory: 16.8 MB (38.01%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    int solve(string p, string q, int i, int j, 
-    vector<vector<int>> &dp)
-    {
-        if(i<0 || j<0)
-        {
-            return 0;
-        }
-
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-
-        if(p[i]==q[j])
-        {
-            // can take
-            return dp[i][j] = 1+solve(p,q,i-1,j-1,dp);
-        }
-        else
-        {
-            return dp[i][j] = max(solve(p,q,i-1,j,dp), solve(p,q,i,j-1,dp));
-        }
-    }
     int minDistance(string p, string q) {
         int m = p.length();
         int n = q.length();
 
-        vector<vector<int>> dp(m, vector<int> (n,-1));
-        return (m+n) - 2*solve(p,q,m-1,n-1,dp);
+        vector<vector<int>> dp(m+1, vector<int> (n+1,0));
+        for(int i=1; i<=m; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                if(p[i-1]==q[j-1])
+                {
+                    // take
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return (m+n) - 2*dp[m][n];
     }
 };
