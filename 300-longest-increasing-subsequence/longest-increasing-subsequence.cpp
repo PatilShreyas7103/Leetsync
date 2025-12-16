2d271ae587af15f8ebd84103fc82d73d7efe5fbe
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-16 12:21:46 +0530

    Time: 268 ms (32.25%) | Memory: 296.4 MB (24.56%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,21 +1,33 @@
+// Longest Increasing Subsequence - Tabulation
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& v) {
         int n = v.size();
-        vector<int> dp(n,1);
-        int mx = 1;
-        for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<i; j++)
-            {
-                if(v[j]<v[i])
-                {
-                    dp[i] = max(dp[i], 1+dp[j]);
+
+        // dp[i][pre+1]
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        // i goes from n-1 to 0
+        for (int i = n - 1; i >= 0; i--) {
+            // pre goes from i-1 to -1
+            for (int pre = i - 1; pre >= -1; pre--) {
+
+                // option 1: skip current element
+                int notTake = dp[i + 1][pre + 1];
+
+                // option 2: take current element (if valid)
+                int take = 0;
+                if (pre == -1 || v[i] > v[pre]) {
+                    take = 1 + dp[i + 1][i + 1];
                 }
+
+                dp[i][pre + 1] = max(take, notTake);
             }
-            mx = max(mx, dp[i]);
         }
 
-        return mx;
+        return dp[0][0]; // i = 0, pre = -1
     }
 };
