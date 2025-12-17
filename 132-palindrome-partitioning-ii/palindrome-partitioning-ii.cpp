c3ef2ba816f0018ba27ccb50c40dd15c18cd8549
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-17 18:51:40 +0530

    Time: 1754 ms (10.72%) | Memory: 9.2 MB (92.94%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -13,30 +13,25 @@ public:
         }
         return true;
     }
-    int solve(int i,int n, string &s, vector<int> &dp)
-    {
-        if(i==n)
-        {
-            return  0;
-        }
-        if(dp[i]!=-1)
-        {
-            return dp[i];
-        }
-        int ans = n;
-        for(int j=i; j<n; j++)
-        {
-            if(palin(i,j,s))
-            {
-                int cut = 1+ solve(j+1,n,s,dp);
-                ans=min(ans, cut);
-            }
-        }
-        return dp[i] =  ans;
-    }
     int minCut(string s) {
         int n = s.length();
-        vector<int> dp(n,-1);
-        return solve(0,n,s,dp) -1;
+        vector<int> dp(n+1,0);
+
+        for(int i=n-1; i>=0; i--)
+        {
+            int ans = n;
+            for(int j=i; j<n; j++)
+            {
+                if(palin(i,j,s))
+                {
+                    int cut = 1+ dp[j+1];
+                    ans=min(ans, cut);
+                }
+            }
+            dp[i] =  ans;
+        }
+
+        return dp[0]-1;
+        
     }
 };
