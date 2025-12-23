2f855fdd70a0c96bff619a704a4df3057269b2bb
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-23 20:26:51 +0530

    Time: 108 ms (48.61%) | Memory: 87.9 MB (48.15%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -1,26 +1,18 @@
 class Solution {
-    private:
-    int dfs(int i, string& s, unordered_set<string>& words, vector<int>& dp) {
-        int n = s.length();
-        if(i==n)
-        {
-            return dp[i] = 0;
-        }
-        if (dp[i] != -1) return dp[i];
-        int res = 1 + dfs(i + 1, s, words, dp);
-        for (int j = i; j < s.size(); j++) {
-            if (words.count(s.substr(i, j - i + 1))) {
-                res = min(res, dfs(j + 1, s, words, dp));
-            }
-        }
-        dp[i] = res;
-        return res;
-    }
 public:
     int minExtraChar(string s, vector<string>& dictionary) {
         unordered_set<string> words(dictionary.begin(), dictionary.end());
         int n = s.size();
-        vector<int> dp(n + 1, -1);
-        return dfs(0, s, words, dp);
+        vector<int> dp(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = 1 + dp[i + 1];
+            for (int j = i; j < n; j++) {
+                if (words.count(s.substr(i, j - i + 1))) {
+                    dp[i] = min(dp[i], dp[j + 1]);
+                }
+            }
+        }
+        return dp[0];
     }
 };
