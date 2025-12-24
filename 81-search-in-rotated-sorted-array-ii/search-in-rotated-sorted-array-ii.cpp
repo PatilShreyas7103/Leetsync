f88cddce77cafc035c302d7df633811755ed962d
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-24 22:37:47 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (55.12%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,11 +1,57 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int size = nums.size();
-
-        for (int index = 0; index < size; index++) {
-            if (nums[index] == target) {
+        int n = nums.size();
+        if(n==1)
+        {
+            if(nums[0]==target)
+            {
                 return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        int s = 0;
+        int e = n-1;
+        while(s<=e)
+        {
+            int mid = (s+e)/2;
+            // Left part is sorted
+            if(nums[mid]==target)
+            {
+                return true;
+            }
+            if (nums[s] == nums[mid] && nums[mid] == nums[e]) 
+            {
+                s = s + 1;
+                e = e - 1;
+                continue;
+            }
+
+            if(nums[s]<=nums[mid])
+            {
+                if(nums[s]<=target && target<=nums[mid])
+                {
+                    e = mid-1;
+                }
+                else
+                {
+                    s = mid+1;
+                }
+            }
+            else
+            {
+                // Right part is sorted
+                if(nums[mid]<=target && target<=nums[e])
+                {
+                    s = mid+1;
+                }
+                else
+                {
+                    e = mid-1;
+                }
             }
         }
 
