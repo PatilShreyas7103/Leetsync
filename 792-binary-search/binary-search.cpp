865ec15a30827ee4c9fe3b233674504e3a9347bc
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-24 19:09:16 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (44.87%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
+    int solve(int s, int e, vector<int> &v, int t)
+    {
+        if(s>e)
+        {
+            return -1;
+        }
+
+        int mid = (s+e)/2;
+        if(v[mid]==t)
+        {
+            return mid;
+        }
+        else if(t>v[mid])
+        {
+            return solve(mid+1,e,v,t);
+        }
+        else
+        {
+            return solve(s,mid-1,v,t);
+        }
+    }
     int search(vector<int>& v, int t) {
         int n = v.size();
-
         int s = 0;
         int e = n-1;
 
-        while(s<=e)
-        {
-            int mid = (s+e)/2;
-            if(v[mid]==t)
-            {
-                return mid;
-            }
-            else if(t>v[mid])
-            {
-                s = mid+1;
-            }
-            else
-            {
-                e = mid-1;
-            }
-        }
-
-        return -1;
+        int ans = solve(s,e,v,t);
+        return ans;
     }
 };
