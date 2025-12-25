a1425780a200d9609346d164bfef6f90487dc8a4
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-25 11:01:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (84.47%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x==0 || x==1)
-        {
-            return x;
-        }
-        long long ans = 1;
-        for(int i=1; i<=x; i++)
-        {
-            long long sq = (long long)i*i;
-            if(sq==x)
-            {
-                return i;
-            }
-            else if(sq<x)
-            {
-                ans = i;
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        return ans;
+        return sqrt(x);
     }
 };
