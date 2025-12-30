a18a82524fd1dc74494eaa8c906b48c340863ee5
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-30 20:30:01 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (78.95%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,46 +1,50 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
-        vector<int> v;
-        int i = 0;
-        int j = 0;
-
         int m = a.size();
         int n = b.size();
 
-        while(i<m && j<n)
+        if(m>n)
         {
-            if(a[i]<b[j])
+            return findMedianSortedArrays(b,a);
+        }
+        int s = 0;
+        int e = m;
+        int middle = (m+n+1)/2;
+        int tot = m+n;
+
+        while(s<=e)
+        {
+            int cut1 = (s+e)/2;
+            int cut2 = middle-cut1;
+
+            int l1 = cut1==0 ? INT_MIN : a[cut1-1];
+            int l2 = cut2==0 ? INT_MIN : b[cut2-1];
+
+            int r1 = cut1==m ? INT_MAX : a[cut1];
+            int r2 = cut2==n ? INT_MAX : b[cut2];
+
+            if(l1<=r2 && l2<=r1)
             {
-                v.push_back(a[i]);
-                i++;
+                if(tot&1){
+                    return double(max(l1,l2));
+                }
+                else
+                {
+                    return double(max(l1,l2)+min(r1,r2))/2.0;
+                }
+            }
+            else if(l1>r2)
+            {
+                e = cut1-1;
             }
             else
             {
-                v.push_back(b[j]);
-                j++;
+                s = cut1+1;
             }
+            
         }
 
-        while(i<m)
-        {
-            v.push_back(a[i]);
-            i++;
-        }
-        while(j<n)
-        {
-            v.push_back(b[j]);
-            j++;
-        }
-
-        int sz = v.size();
-        if(sz&1)
-        {
-            return v[sz/2];
-        }
-        else
-        {
-            return (double)(v[sz/2]+v[sz/2-1])/2.0;
-        }
+        return 0.0;
     }
 };
