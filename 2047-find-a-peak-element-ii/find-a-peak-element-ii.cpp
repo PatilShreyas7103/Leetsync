033527b48bddf5ea6718b53ba4747c4c87a57214
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-30 13:53:48 +0530

    Time: 0 ms (100.00%) | Memory: 50.6 MB (31.78%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -1,46 +1,24 @@
 class Solution {
 public:
-    bool isValid(int r, int c, int m, int n)
-    {
-        return (r>=0 && c>=0 && r<m && c<n);
-    }
     vector<int> findPeakGrid(vector<vector<int>>& v) {
         int m = v.size();
         int n = v[0].size();
-        
-        int dx[] = {-1,0,1,0};
-        int dy[] = {0,1,0,-1};
+        int mx = -1;
+        int r = -1;
+        int c =  -1;
 
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
             {
-                bool f = true;
-                for(int k=0; k<4; k++)
+                if(v[i][j]>mx)
                 {
-                    int adjr = i+dx[k];
-                    int adjc = j+dy[k];
-
-                    if(isValid(adjr,adjc,m,n))
-                    {
-                        int adval = v[adjr][adjc];
-                        int val = v[i][j];
-
-                        if(adval>val)
-                        {
-                            f = false;
-                            break;
-                        }
-                    }
-                }
-                if(f)
-                {
-                    return {i,j};
+                    mx = v[i][j];
+                    r = i;
+                    c = j;
                 }
             }
         }
-
-
-        return {-1,-1};
+        return {r,c};
     }
 };
