bf9d8d796ee0725f1e10157206b15a2e973b27c3
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-30 13:05:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (75.11%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,51 +4,22 @@ public:
         int m = v.size();
         int n = v[0].size();
 
-        int i= 0;
-        int j = m-1;
-        bool f = false;
-        int row = -1;
-
-        while(i<=j)
-        {
-            int mid = (i+j)/2;
-
-            int s = v[mid][0];
-            int e = v[mid][n-1];
-
-            if(t>=s && t<=e)
-            {
-                // found row
-                f = true;
-                row = mid;
-                break;
-            }
-            else if(t<s)
-            {
-                j = mid-1;
-            }
-            else
-            {
-                i = mid+1;
-            }
-        }
-
-        if(!f){
-            return false;
-        }
-        int s =0;
-        int e = n-1;
+        int s = 0;
+        int e = n*m-1;
 
         while(s<=e)
         {
             int mid = (s+e)/2;
-            int element = v[row][mid];
 
-            if(element==t)
+            int row = mid/n;
+            int col = mid%n;
+            int x = v[row][col];
+
+            if(t==x)
             {
                 return true;
             }
-            else if(element<t)
+            else if(t>x)
             {
                 s = mid+1;
             }
