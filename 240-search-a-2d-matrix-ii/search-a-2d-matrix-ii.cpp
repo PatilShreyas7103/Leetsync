384755aa55cf87ff331699e77561f47f072ce77d
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2025-12-30 13:16:25 +0530

    Time: 675 ms (5.08%) | Memory: 18.6 MB (66.76%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& v, int t) {
+        int m = v.size(), n = v[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (v[i][j] == t) return true;
+            }
+        }
+        return false;
+    }
+};
