ee0af449acb2e4be3fd6d0cb1c08be9c6f8f1a00
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-01-14 21:49:13 +0530

    Time: 51 ms (23.78%) | Memory: 21.7 MB (15.76%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -23,7 +23,21 @@ public:
     }
     int jump(vector<int>& v) {
         int n = v.size();
-        vector<int> dp(n,-1);
-        return solve(0,v,n,dp);
+        vector<int> dp(n+1,0);
+
+        for(int i=n-2; i>=0; i--)
+        {
+            int start = i+1;
+            int end = min(n-1,i+v[i]);
+            long long ans = INT_MAX;
+            for(int j=start;j<=end; j++)
+            {
+                long long res = (long long)1+dp[j];
+                ans=min(ans,res);
+            }
+            dp[i] = ans;
+        }
+
+        return dp[0];
     }
 };
