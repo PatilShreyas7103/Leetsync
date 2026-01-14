6bfcaa45bf5d25576dee80d1ce028087d28d262c
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-01-14 21:49:22 +0530

    Time: 46 ms (24.36%) | Memory: 21.6 MB (15.76%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,26 +1,6 @@
 class Solution {
 public:
-    int solve(int i, vector<int> &v, int n, vector<int> &dp)
-    {
-        if(i==n-1)
-        {
-            return dp[i] = 0;
-        }
-        if(dp[i]!=-1)
-        {
-            return dp[i];
-        }
-        int start = i+1;
-        int end = min(n-1,i+v[i]);
-        long long ans = INT_MAX;
-        for(int j=start;j<=end; j++)
-        {
-            long long res = (long long)1+solve(j,v,n,dp);
-            ans=min(ans,res);
-        }
 
-        return dp[i] = ans;
-    }
     int jump(vector<int>& v) {
         int n = v.size();
         vector<int> dp(n+1,0);
