7d2a5865dab1260ce18da43c448fbd7f9f6efabc
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-01-15 19:29:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (58.11%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -2,37 +2,15 @@ class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& v, int e) {
         int n = v.size();
-        vector<bool> ans(n,true);
+        vector<bool> ans(n, true);
+
         int mx = -1;
-        int sec = -1;
+        for (int x : v)
+            mx = max(mx, x);
 
-        for(int i=0; i<n; i++)
-        {
-            if(v[i]>mx)
-            {
-                sec = mx;
-                mx = v[i];
-            }
-            else if(v[i]<mx && v[i]>sec)
-            {
-                sec = v[i];
-            }
-        }
-
-        for(int i=0; i<n; i++)
-        {
-            if(v[i]==mx)
-            {
-                ans[i] = 1;
-            }
-            else
-            {
-                int val = v[i]+e;
-                if(mx>val)
-                {
-                    ans[i] = 0;
-                }
-            }
+        for (int i = 0; i < n; i++) {
+            if (v[i] + e < mx)
+                ans[i] = false;
         }
 
         return ans;
