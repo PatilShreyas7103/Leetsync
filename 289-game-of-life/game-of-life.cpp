df5f2b7b3fbd8958f4cf94abc0c0380f74510148
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-01-15 18:21:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (84.24%) - LeetSync

--- 289-game-of-life/game-of-life.cpp
+++ 289-game-of-life/game-of-life.cpp
@@ -1,85 +1,51 @@
+
 class Solution {
 public:
-    bool isValid(int r,int c, int m, int n)
-    {
-        return (r>=0 && c>=0 && r<m && c<n);
+    bool isValid(int r, int c, int m, int n) {
+        return (r >= 0 && c >= 0 && r < m && c < n);
     }
 
-    void gameOfLife(vector<vector<int>>& v) {
-        int m = v.size();
-        int n = v[0].size();
+    void gameOfLife(vector<vector<int>>& board) {
+        int m = board.size();
+        int n = board[0].size();
 
-        vector<vector<int>> ans(m, vector<int> (n,0));
+        // Direction vectors
+        int dx[8] = {-1,-1,-1,0,0,1,1,1};
+        int dy[8] = {-1,0,1,-1,1,-1,0,1};
 
-        for(int i=0; i<m; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                if(v[i][j]==1)
-                {
-                    // live cell
-                    int cnt = -1;
-                    for(int a=-1; a<=1; a++)
-                    {
-                        for(int b=-1; b<=1; b++)
-                        {
-                            int r = i+a;
-                            int c = j+b;
-                        
-                            if(isValid(r,c,m,n))
-                            {
-                                if(v[r][c]==1)
-                                {
-                                    cnt++;
-                                }
-                            }
-                        }
-                    }
-                    if(cnt<2)
-                    {
-                        ans[i][j] = 0;
-                    }
-                    else if(cnt>3)
-                    {   
-                        ans[i][j] = 0;
-                    }
-                    else
-                    {
-                        // lives
-                        ans[i][j] = 1;
+        // First pass: mark transitions
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int live = 0;
+
+                for (int d = 0; d < 8; d++) {
+                    int r = i + dx[d];
+                    int c = j + dy[d];
+
+                    if (isValid(r, c, m, n) &&
+                        (board[r][c] == 1 || board[r][c] == -1)) {
+                        live++;
                     }
                 }
-                else
-                {
-                    int cnt = 0;
-                    // dead cell
-                    for(int a=-1; a<=1; a++)
-                    {
-                        for(int b=-1; b<=1; b++)
-                        {
-                            int r = i+a;
-                            int c = j+b;
-                            
-                            if(isValid(r,c,m,n))
-                            {
-                                if(v[r][c]==1)
-                                {
-                                    cnt++;
-                                }
-                            }
-                        }
-                    }
-                    if(cnt==3)
-                    {
-                        ans[i][j] = 1;
-                    }
-                    else
-                    {
-                        ans[i][j] = 0;
-                    }
+
+                if (board[i][j] == 1) {
+                    if (live < 2 || live > 3)
+                        board[i][j] = -1; // live → dead
+                } else {
+                    if (live == 3)
+                        board[i][j] = 2;  // dead → live
                 }
             }
         }
-        v = ans;
+
+        // Second pass: finalize states
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == -1)
+                    board[i][j] = 0;
+                else if (board[i][j] == 2)
+                    board[i][j] = 1;
+            }
+        }
     }
 };
