ffd157ffc6350cb432322988ad6e3bd4a6499c42
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-01-19 02:27:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (78.37%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -1,53 +1,9 @@
 class Solution {
 public:
-    bool valid(string ans, string s, string t, int m, int n)
-    {
-        int sz = ans.length();
-        if(sz>m || sz>n)
-        {
-            return false;
-        }
-        int r1 = (m+sz-1)/sz;
-        int r2 = (n+sz-1)/sz;
-
-        string k = "";
-        for(int i=1; i<=r1; i++)
-        {
-            k+=ans;
-        }
-        if(k!=s)
-        {
-            return false;
-        }
-        k = "";
-        for(int i=1; i<=r2; i++)
-        {
-            k+=ans;
-        }
-        if(k!=t)
-        {
-            return false;
-        }
-
-        return true;
-    }
     string gcdOfStrings(string s, string t) {
-        int m = s.length();
-        int n = t.length();
+        if (s + t != t + s) return "";
 
-        string ans = "";
-        string res = "";
-
-        for(int i=0; i<n; i++)
-        {
-            ans+=t[i];
-            if(valid(ans,s,t,m,n))
-            {
-                res = ans;
-            }
-        }
-
-        return res;
-
+        int g = gcd(s.size(), t.size());
+        return s.substr(0, g);
     }
 };
