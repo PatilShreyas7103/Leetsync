b4764a3a4689ead686b3d8aeeff17389b08e565a
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-01-20 12:44:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (31.80%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,38 +1,61 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string temp = "";
-        for(auto it: s)
-        {
-            if(it>='a' && it<='z')
-            {
-                // lowercase
-                temp+=it;
-            }
-            else if(it>='A' && it<='Z')
-            {
-                // uppercase
-                temp+= it+32;
-            }
-            else if(it>='0' && it<='9')
-            {
-                // skip
-                temp+=it;
-            }
-        }
-        int n = temp.length();
+        int n = s.length();
         int i = 0;
         int j = n-1;
 
         while(i<=j)
         {
-            if(temp[i]!=temp[j])
+            if(s[i]>='a' && s[i]<='z')
+            {
+                // lowercase
+            }
+            else if(s[i]>='A' && s[i]<='Z')
+            {
+                // convert
+                s[i]+=32;
+            }   
+            else if(s[i]>='0' && s[i]<='9')
+            {
+                // number
+            }
+            else
+            {
+                i++;
+                continue;
+            }
+
+            if(s[j]>='a' && s[j]<='z')
+            {
+                // lowercase
+            }
+            else if(s[j]>='A' && s[j]<='Z')
+            {
+                // convert
+                s[j]+=32;
+            }   
+            else if(s[j]>='0' && s[j]<='9')
+            {
+                // number
+            }
+            else
+            {
+                j--;
+                continue;
+            }
+            if(i>j)
+            {
+                break;
+            }
+            if(s[i]!=s[j])
             {
                 return false;
             }
             i++;
             j--;
         }
+
         return true;
     }
 };
