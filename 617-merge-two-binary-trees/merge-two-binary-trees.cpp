55d5159f1109f3d49d067e8d07c50a968db7a2d7
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-01-25 12:42:32 +0530

    Time: 0 ms (100.00%) | Memory: 33.1 MB (75.59%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/merge-two-binary-trees.cpp
@@ -1,46 +1,13 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* solve(TreeNode* p, TreeNode* q)
-    {
-        if(!p && !q)
-        {
-            return NULL;
-        }
-
-        if(p && !q)
-        {
-            TreeNode* temp = new TreeNode(p->val);
-            temp->left = solve(p->left,q);
-            temp->right = solve(p->right,q);
-            return temp;
-        }
-
-        if(q && !p)
-        {
-            TreeNode* temp = new TreeNode(q->val);
-            temp->left = solve(p,q->left);
-            temp->right = solve(p,q->right);
-            return temp;
-        }
-
-        TreeNode* temp = new TreeNode(p->val+q->val);
-        temp->left = solve(p->left,q->left);
-        temp->right = solve(p->right,q->right);
-
-        return temp;
-    }
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        return solve(root1,root2);
+        if (!root1) return root2;
+        if (!root2) return root1;
+
+        root1->val += root2->val;
+        root1->left = mergeTrees(root1->left, root2->left);
+        root1->right = mergeTrees(root1->right, root2->right);
+
+        return root1;
     }
 };
