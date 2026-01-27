cce548d98953f83a8a3078195b15ea18be3eb872
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-01-28 01:46:43 +0530

    Time: 1 ms (33.09%) | Memory: 52.5 MB (21.09%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& v) {
+        int n = v.size();
+        int mx = 0;
+
+        for(int i=0; i<n; i++)
+        {
+            if(i>mx)
+            {
+                return false;
+            }
+            mx = max(mx,v[i]+i);
+        }
+
+        return true;
+    }
+};
