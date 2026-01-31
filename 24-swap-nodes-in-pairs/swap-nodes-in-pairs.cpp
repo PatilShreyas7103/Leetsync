60a5d700cf0621096328153eba14fb08a77acbb2
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-01-31 19:25:17 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (19.40%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -1,57 +1,21 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        if(!head)
-        {
-            return head;
-        }
-        if(head->next==NULL)
-        {
-            // single node
-            return  head;
-        }
-        if(head->next->next==NULL)
-        {
-            // two nodes
-            ListNode* temp = head->next;
-            temp->next = head;
-            head->next = NULL;
-            return temp;
+        ListNode dummy(0, head);
+        ListNode *prev = &dummy, *cur = head;
+
+        while (cur && cur->next) {
+            ListNode *npn = cur->next->next;
+            ListNode *second = cur->next;
+
+            second->next = cur;
+            cur->next = npn;
+            prev->next = second;
+
+            prev = cur;
+            cur = npn;
         }
 
-        ListNode* temp = head;
-        ListNode* curr = head->next;
-
-        while(curr)
-        {
-            int k = temp->val;
-            int k2 = curr->val;
-            temp->val = k2;
-            curr->val = k;
-            if(temp->next)
-            {
-                temp = temp->next->next;
-            }
-            if(curr->next)
-            {
-                curr = curr->next->next;
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        return head;
+        return dummy.next;        
     }
 };
