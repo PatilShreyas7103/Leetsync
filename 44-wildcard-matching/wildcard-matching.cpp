890d0e2d3e0cda6f93f445d8d65df259641d3f8c
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-04-02 22:00:32 +0530

    Time: 39 ms (71.19%) | Memory: 54.3 MB (24.91%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool solve (int i, int j, string &s, string &p)
+    bool solve (int i, int j, string &s, string &p,
+    vector<vector<int>> &dp)
     {
         if(i<0)
         {
@@ -19,27 +20,32 @@ public:
             return false;
         }
 
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+
         if(p[j]=='*')
         {
-            bool op1 = solve(i-1,j-1,s,p);
-            bool op2 = solve(i,j-1,s,p);
-            bool op3 = solve(i-1,j,s,p);
+            bool op1 = solve(i-1,j-1,s,p,dp);
+            bool op2 = solve(i,j-1,s,p,dp);
+            bool op3 = solve(i-1,j,s,p,dp);
 
-            return op1 || op2 || op3;
+            return dp[i][j] =  op1 || op2 || op3;
         }
         else if(p[j]=='?')
         {
-            return solve(i-1,j-1,s,p);
+            return dp[i][j] =  solve(i-1,j-1,s,p,dp);
         }
         else
         {
             if(s[i]!=p[j])
             {
-                return false;
+                return dp[i][j] =  false;
             }
             else
             {
-                return solve(i-1,j-1,s,p);
+                return dp[i][j] =  solve(i-1,j-1,s,p,dp);
             }
         }
     }
@@ -48,47 +54,7 @@ public:
         int m = s.length();
         int n = p.length();
 
-        vector<vector<int>> dp(m+1, vector<int> (n+1,0));
-        bool f = true;
-        for(int j=0; j<=n; j++)
-        {
-            
-            dp[0][j] = f;
-            if(p[j]!='*')
-            {
-                f = false;
-            }
-        }
-
-        for(int i=1; i<=m; i++)
-        {
-            for(int j=1; j<=n; j++)
-            {
-                if(p[j-1]=='*')
-                {
-                    bool op1 = dp[i-1][j-1];
-                    bool op2 = dp[i-1][j];
-                    bool op3 = dp[i][j-1];
-
-                    dp[i][j] =  op1 || op2 || op3;
-                }
-                else if(p[j-1]=='?')
-                {
-                    dp[i][j] =  dp[i-1][j-1];
-                }
-                else
-                {
-                    if(s[i-1]!=p[j-1])
-                    {
-                        dp[i][j] =  false;
-                    }
-                    else
-                    {
-                        dp[i][j] =  dp[i-1][j-1];
-                    }
-                }
-            }
-        }
-        return dp[m][n];
+        vector<vector<int>> dp(m, vector<int> (n,-1));
+        return solve(m-1,n-1,s,p,dp);
     }
 };
