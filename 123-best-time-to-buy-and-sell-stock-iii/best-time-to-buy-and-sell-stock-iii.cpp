e924df60074044ec4772fb30164c0311add4c468
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-04-03 09:48:00 +0530

    Time: 445 ms (26.07%) | Memory: 223.8 MB (26.27%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& p) {
-        int n = p.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
-
-        for(int i=n-1; i>=0; i--)
+    int solve(int i, int buy, int k, vector<int> &v, int n, vector<vector<vector<int>>> &dp)
+    {
+        if(i==n || k==2)
         {
-            for(int t=2; t>0; t--)
-            {
-                dp[i][1][t] = max(dp[i+1][1][t], -p[i]+dp[i+1][0][t]);
-                dp[i][0][t] = max(dp[i+1][0][t], p[i]+dp[i+1][1][t-1]); 
-            }
-                   
+            return 0;
         }
 
-        return dp[0][1][2];
+        if(dp[i][buy][k]!=-1)
+        {
+            return dp[i][buy][k];
+        }
+
+        int dont = solve(i+1,buy,k,v,n,dp);
+        int take = INT_MIN;
+        if(buy==1)
+        {
+            take = -v[i]+solve(i+1,0,k,v,n,dp);
+        }
+        else
+        {
+            take = v[i]+solve(i+1,1,k+1,v,n,dp);
+        }
+
+        return dp[i][buy][k] =  max(dont,take);
+    }
+    int maxProfit(vector<int>& v) {
+        int n = v.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>> (2, vector<int> (2,-1)));
+
+        return solve(0,1,0,v,n,dp);
+
     }
 };
