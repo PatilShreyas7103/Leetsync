770c27de5add3c5a1487c4ae6af6eccfd0188664
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-04-03 12:12:00 +0530

    Time: 60 ms (58.57%) | Memory: 14.4 MB (43.61%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
-    int solve(int i, int prev, vector<int> &v, int n,
-    vector<vector<int>> &dp)
-    {
-        if(i==n)
-        {
-            return 0;
-        }
-
-        
-        if(dp[i][prev+1]!=-1)
-        {
-            return dp[i][prev+1];
-        }
-        
-
-        int dont = solve(i+1,prev,v,n,dp);
-        int take = 0;
-        if(prev==-1)
-        {
-            take = 1+solve(i+1,i,v,n,dp);
-        }
-        else
-        {
-            if(v[i]>v[prev])
-            { 
-                // valid
-                take = 1+solve(i+1,i,v,n,dp);
-            }
-        }
-
-        return dp[i][prev+1] =  max(take,dont);
-    }
     int lengthOfLIS(vector<int>& v) {
         int n = v.size();
-        vector<vector<int>> dp(n+1, vector<int> (n+1,-1));
-        return solve(0,-1,v,n,dp);
+        int ans = 0;
+        vector<int> dp(n,1);
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<i; j++)
+            {
+                if(v[i]>v[j])
+                {
+                    dp[i] = max(dp[i], 1+dp[j]);
+                }
+            }
+            ans=max(ans, dp[i]);
+        }
+
+        return ans;
     }
 };
