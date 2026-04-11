33b8f5b875451733b649b8dbc672202b9879b15c
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-04-11 15:59:27 +0530

    Time: 3 ms (73.18%) | Memory: 11.3 MB (76.40%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
-    bool solve(int i, string s, set<string> &st, int n,
-    vector<int> &dp)
-    {
-        if(i==n)
-        {
-            return true;
-        }
-        if(dp[i]!=-1)
-        {
-            return dp[i];
-        }
-
-        bool f = false;
-        string curr = "";
-        for(int j=i; j<n; j++)
-        {
-            curr+= s[j];
-            if(st.find(curr)!=st.end())
-            {
-                bool c = solve(j+1,s,st,n,dp);
-                f = f | c;
-            }
-        }
-
-        return dp[i] =  f;
-    }
     bool wordBreak(string s, vector<string>& v) {
         set<string> st(v.begin(), v.end());
         int n = s.length();
-        vector<int> dp(n,-1);
+        vector<int> dp(n+1,0);
+        dp[n] = 1;
 
-        return solve(0,s,st,n,dp);
+        for(int i=n-1; i>=0; i--)
+        {
+            bool f = false;
+            string curr = "";
+            for(int j=i; j<n; j++)
+            {
+                curr+= s[j];
+                if(st.find(curr)!=st.end())
+                {
+                    bool c = dp[j+1];
+                    f = f | c;
+                }
+            }
+
+            dp[i] =  f;
+        }
+
+        return dp[0];
     }
 };
