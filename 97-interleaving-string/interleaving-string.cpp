5e7eca5409038bde88f38bc0d2397bec9dd73de4
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-04-13 12:17:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (36.16%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,55 +1,34 @@
+
 class Solution {
 public:
-    int solve(int i, int j, int k, string &s1, string &s2, string &s3,
-    int x, int y, int z, vector<vector<vector<int>>> &dp)
-    {
-        if(k==z)
-        {
-            if(i==x && j==y)
-            {
-                // all end
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+    bool solve(int i, int j, string &s1, string &s2, string &s3, vector<vector<int>> &dp) {
+        if(i == s1.length() && j == s2.length())
+            return true;
 
-        if(dp[i][j][k]!=-1)
-        {
-            return dp[i][j][k];
-        }
+        if(dp[i][j] != -1)
+            return dp[i][j];
 
-        // both, one, none
-        char a = s1[i];
-        char b = s2[j];
-        char c = s3[k];
+        int k = i + j;
 
-        if(i<x && j<y && a==c && b==c)
-        {
-            return dp[i][j][k]= (solve(i+1,j,k+1,s1,s2,s3,x,y,z,dp) || solve(i,j+1,k+1,s1,s2,s3,x,y,z,dp));
-        }
-        else if(i<x && a==c)
-        {
-            return dp[i][j][k]= solve(i+1,j,k+1,s1,s2,s3,x,y,z,dp);
-        }
-        else if(j<y && b==c)
-        {
-            return dp[i][j][k]= solve(i,j+1,k+1,s1,s2,s3,x,y,z,dp);
-        }
-        else
-        {
-            return dp[i][j][k]= false;
-        }
+        bool ans = false;
+
+        if(i < s1.length() && s1[i] == s3[k])
+            ans = ans || solve(i + 1, j, s1, s2, s3, dp);
+
+        if(j < s2.length() && s2[j] == s3[k])
+            ans = ans || solve(i, j + 1, s1, s2, s3, dp);
+
+        return dp[i][j] = ans;
     }
+
     bool isInterleave(string s1, string s2, string s3) {
         int x = s1.length();
         int y = s2.length();
-        int z = s3.length();
 
-        vector<vector<vector<int>>> dp(x+1, vector<vector<int>> (y+1, vector<int> (z+1,-1)));
+        if(x + y != s3.length()) return false;
 
-        return solve(0,0,0,s1,s2,s3,x,y,z,dp);
+        vector<vector<int>> dp(x + 1, vector<int>(y + 1, -1));
+
+        return solve(0, 0, s1, s2, s3, dp);
     }
 };
