e3317da3415ad5966914d87751502f535ec03014
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-04-14 12:52:50 +0530

    Time: 34 ms (41.87%) | Memory: 36.7 MB (39.54%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -21,11 +21,26 @@ public:
 
         return dp[i][t] =  take+dont;
     }
-    int change(int t, vector<int>& v) {
+    int change(int t1, vector<int>& v) {
         int n = v.size();
-        vector<vector<int>> dp(n, vector<int> (t+1,-1));
+        vector<vector<int>> dp(n+1, vector<int> (t1+1,0));
+        dp[0][0] = 1;
 
-        return solve(n-1,t,v,dp);
+        for(int i=1; i<=n; i++)
+        {
+            for(int t=0; t<=t1; t++)
+            {
+                int dont = dp[i-1][t];
+                int take = 0;
+                if(v[i-1]<=t)
+                {
+                    take = dp[i][t-v[i-1]];
+                }
 
+                dp[i][t] =  (long long)take+dont;
+            }
+        }
+
+        return dp[n][t1];
     }
 };
