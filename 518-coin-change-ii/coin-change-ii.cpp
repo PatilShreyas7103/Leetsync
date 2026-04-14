f9f884ebad19dddbe0746fd9488808223702da2b
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-04-14 12:56:42 +0530

    Time: 31 ms (48.58%) | Memory: 36.5 MB (53.79%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,26 +1,6 @@
 class Solution {
 public:
-    int solve(int i, int t, vector<int> &v, 
-    vector<vector<int>> &dp)
-    {
-        if(i<0)
-        {
-            return t==0;
-        }
-        if(dp[i][t]!=-1)
-        {
-            return dp[i][t];
-        }
 
-        int dont = solve(i-1,t,v,dp);
-        int take = 0;
-        if(v[i]<=t)
-        {
-            take = solve(i,t-v[i],v,dp);
-        }
-
-        return dp[i][t] =  take+dont;
-    }
     int change(int t1, vector<int>& v) {
         int n = v.size();
         vector<vector<int>> dp(n+1, vector<int> (t1+1,0));
