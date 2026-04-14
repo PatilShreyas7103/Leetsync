762fb2c1f51842be2d73e5085a809537a3e62dc0
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-04-14 22:38:01 +0530

    Time: 20 ms (5.53%) | Memory: 19.7 MB (7.70%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,9 +1,29 @@
 class Solution {
 public:
-    // int solve()
+    int solve(int l, int r, vector<int> &v,int sum,
+    vector<vector<int>> &dp)
+    {
+        if(l>r)
+        {
+            return 0;
+        }
+
+        if(dp[l][r]!=-1)
+        {
+            return dp[l][r];
+        }
+
+        int even = (r-l)%2==0;
+        int left = even ? v[l] : 0;
+        int right = even ? v[r] : 0;
+
+        return dp[l][r] = max(left+solve(l+1,r,v,sum,dp), right+solve(l,r-1,v,sum,dp));
+    }
     bool stoneGame(vector<int>& v) {
-        // int total = 0;
-        return true;
-        
+        int n = v.size();
+        int sum = accumulate(v.begin(),v.end(),0);
+        vector<vector<int>> dp(n, vector<int> (n,-1));
+        int alice = solve(0,n-1,v,sum,dp);
+        return alice > (sum-alice);
     }
 };
