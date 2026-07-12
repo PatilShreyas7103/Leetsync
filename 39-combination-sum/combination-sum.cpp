14eb1c6ff4e771cb391859e642306efb9ad58eb4
Shreyas Patil <161187040+PatilShreyas7103@users.noreply.github.com>
2026-07-12 23:33:43 +0530

    Time: 59 ms (10.38%) | Memory: 54.6 MB (9.38%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    void solve(int i, int t, vector<int> &v,
-    vector<vector<int>> &ans, vector<int> curr)
+    void solve(int i,vector<int>& v, int t, vector<int> ans, vector<vector<int>> &res)
     {
-        if(i==0)
+        if(t==0)
         {
-            if(t%v[0]==0)
-            {
-                int f = t/v[0];
-                while(f--)
-                {
-                    curr.push_back(v[0]);
-                }
-                ans.push_back(curr);
-                
-            }
+            res.push_back(ans);
+            return;
+        }
+        if(i<0 || t<0)
+        {
             return;
         }
 
-        solve(i-1,t,v,ans,curr);
-        if(v[i]<=t)
-        {
-            curr.push_back(v[i]);
-            solve(i,t-v[i],v,ans,curr);
-            curr.pop_back();
-        }
+
+        solve(i-1,v,t,ans,res);
+        ans.push_back(v[i]);
+        solve(i,v,t-v[i],ans,res);
     }
 
     vector<vector<int>> combinationSum(vector<int>& v, int t) {
-        vector<vector<int>> ans;
-        vector<int> curr;
-        
+        vector<vector<int>> res;
         int n = v.size();
-        solve(n-1,t,v,ans,curr);
+        vector<int> ans;
+         solve(n-1,v,t,ans,res);
 
-        return ans;
+         return res;
     }
 };
